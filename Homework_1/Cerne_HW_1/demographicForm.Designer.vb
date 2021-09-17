7103810ffef458a3ba7c8c780e1a809d156c0b91
ncerne00 <59320969+ncerne00@users.noreply.github.com>
2021-09-17 10:26:09 -0400

    Push

---
+++ Homework_1/Cerne_HW_1/demographicForm.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class demographicForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.fNameBox = New System.Windows.Forms.TextBox()
+        Me.lNameBox = New System.Windows.Forms.TextBox()
+        Me.ageBox = New System.Windows.Forms.TextBox()
+        Me.firstNameLabel = New System.Windows.Forms.Label()
+        Me.lstNameLabel = New System.Windows.Forms.Label()
+        Me.ageLabel = New System.Windows.Forms.Label()
+        Me.outputLabel = New System.Windows.Forms.Label()
+        Me.enterBtn = New System.Windows.Forms.Button()
+        Me.exitBtn = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'fNameBox
+        '
+        Me.fNameBox.Location = New System.Drawing.Point(71, 51)
+        Me.fNameBox.Name = "fNameBox"
+        Me.fNameBox.Size = New System.Drawing.Size(104, 20)
+        Me.fNameBox.TabIndex = 0
+        '
+        'lNameBox
+        '
+        Me.lNameBox.Location = New System.Drawing.Point(71, 77)
+        Me.lNameBox.Name = "lNameBox"
+        Me.lNameBox.Size = New System.Drawing.Size(104, 20)
+        Me.lNameBox.TabIndex = 1
+        '
+        'ageBox
+        '
+        Me.ageBox.Location = New System.Drawing.Point(71, 103)
+        Me.ageBox.Name = "ageBox"
+        Me.ageBox.Size = New System.Drawing.Size(36, 20)
+        Me.ageBox.TabIndex = 2
+        '
+        'firstNameLabel
+        '
+        Me.firstNameLabel.AutoSize = True
+        Me.firstNameLabel.Location = New System.Drawing.Point(12, 54)
+        Me.firstNameLabel.Name = "firstNameLabel"
+        Me.firstNameLabel.Size = New System.Drawing.Size(57, 13)
+        Me.firstNameLabel.TabIndex = 3
+        Me.firstNameLabel.Text = "First Name"
+        '
+        'lstNameLabel
+        '
+        Me.lstNameLabel.AutoSize = True
+        Me.lstNameLabel.Location = New System.Drawing.Point(12, 84)
+        Me.lstNameLabel.Name = "lstNameLabel"
+        Me.lstNameLabel.Size = New System.Drawing.Size(58, 13)
+        Me.lstNameLabel.TabIndex = 4
+        Me.lstNameLabel.Text = "Last Name"
+        '
+        'ageLabel
+        '
+        Me.ageLabel.AutoSize = True
+        Me.ageLabel.Location = New System.Drawing.Point(24, 110)
+        Me.ageLabel.Name = "ageLabel"
+        Me.ageLabel.Size = New System.Drawing.Size(26, 13)
+        Me.ageLabel.TabIndex = 5
+        Me.ageLabel.Text = "Age"
+        '
+        'outputLabel
+        '
+        Me.outputLabel.AutoSize = True
+        Me.outputLabel.BackColor = System.Drawing.Color.White
+        Me.outputLabel.Location = New System.Drawing.Point(213, 51)
+        Me.outputLabel.MinimumSize = New System.Drawing.Size(269, 150)
+        Me.outputLabel.Name = "outputLabel"
+        Me.outputLabel.Size = New System.Drawing.Size(269, 150)
+        Me.outputLabel.TabIndex = 7
+        '
+        'enterBtn
+        '
+        Me.enterBtn.BackColor = System.Drawing.Color.Aquamarine
+        Me.enterBtn.Location = New System.Drawing.Point(72, 222)
+        Me.enterBtn.Name = "enterBtn"
+        Me.enterBtn.Size = New System.Drawing.Size(75, 23)
+        Me.enterBtn.TabIndex = 8
+        Me.enterBtn.Text = "Enter"
+        Me.enterBtn.UseVisualStyleBackColor = False
+        '
+        'exitBtn
+        '
+        Me.exitBtn.Location = New System.Drawing.Point(417, 222)
+        Me.exitBtn.Name = "exitBtn"
+        Me.exitBtn.Size = New System.Drawing.Size(75, 23)
+        Me.exitBtn.TabIndex = 9
+        Me.exitBtn.Text = "Exit"
+        Me.exitBtn.UseVisualStyleBackColor = True
+        '
+        'demographicForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(591, 277)
+        Me.Controls.Add(Me.exitBtn)
+        Me.Controls.Add(Me.enterBtn)
+        Me.Controls.Add(Me.outputLabel)
+        Me.Controls.Add(Me.ageLabel)
+        Me.Controls.Add(Me.lstNameLabel)
+        Me.Controls.Add(Me.firstNameLabel)
+        Me.Controls.Add(Me.ageBox)
+        Me.Controls.Add(Me.lNameBox)
+        Me.Controls.Add(Me.fNameBox)
+        Me.Name = "demographicForm"
+        Me.Text = "Demographic Form"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents fNameBox As TextBox
+    Friend WithEvents lNameBox As TextBox
+    Friend WithEvents ageBox As TextBox
+    Friend WithEvents firstNameLabel As Label
+    Friend WithEvents lstNameLabel As Label
+    Friend WithEvents ageLabel As Label
+    Friend WithEvents outputLabel As Label
+    Friend WithEvents enterBtn As Button
+    Friend WithEvents exitBtn As Button
+End Class
---
+++ Homework_1/Cerne_HW_1/demographicForm.vb
@@ -0,0 +1,114 @@
+﻿Public Class demographicForm
+
+    ' counting number of entries '
+    Dim count As Integer
+    Dim maxSubjects As Integer = 10
+
+    ' initializing the 2-D array ' 
+    Dim demographicTable(,) As Integer
+
+    Private Function GetAge(age As Integer) As String
+        If age < 18 Then
+            Return "Junior"
+        ElseIf age < 60 Then
+            Return "Adult"
+        ElseIf age >= 60 Then
+            Return "Senior"
+        Else
+            Return "Uknown age."
+        End If
+    End Function
+    Private Sub demographicForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        WireEvents()
+    End Sub
+    ' exit button event handler ' 
+    Private Sub exitBtn_Click(sender As Object, e As EventArgs) Handles exitBtn.Click
+        Me.Close()
+    End Sub
+    ' enter button event handler ' 
+    Private Sub enterBtn_Click(sender As Object, e As EventArgs) Handles enterBtn.Click
+        Dim age As Integer, firstName As String, lastName As String
+
+
+        If CheckFName() AndAlso
+            CheckLName() AndAlso
+            CheckAge(age) Then
+            If count >= maxSubjects Then
+                MessageBox.Show("No more entries allowed. Registry is at it's limit.")
+            Else
+                age = Integer.Parse(ageBox.Text)
+                firstName = fNameBox.Text
+                lastName = lNameBox.Text
+
+                count += 1
+
+                outputLabel.Text &= firstName & " " & lastName & ", " & GetAge(age) & vbCrLf
+            End If
+
+
+        End If
+    End Sub
+    Private Sub WireEvents()
+        AddHandler fNameBox.Leave, AddressOf CheckFName
+        AddHandler lNameBox.Leave, AddressOf CheckLName
+        AddHandler ageBox.Leave, AddressOf CheckAgeNoArg
+
+    End Sub
+    Private Function CheckFName() As Boolean
+        Try
+            If fNameBox.Text = "" Then
+                Throw New Exception("Error: no entry for first name.")
+            ElseIf IsNumeric(fNameBox.Text) Then
+                Throw New Exception("Error: enter a string into first name box.")
+            End If
+            Return True
+        Catch ex As FormatException
+            MessageBox.Show("Error in the First Name.", ex.GetType.ToString)
+            fNameBox.Focus()
+            Return False
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Input Error")
+            fNameBox.Focus()
+            Return False
+        End Try
+    End Function
+    Private Function CheckLName() As Boolean
+        Try
+            If lNameBox.Text = "" Then
+                Throw New Exception("Error: no entry for last name.")
+            ElseIf IsNumeric(lNameBox.Text) Then
+                Throw New Exception("Error: enter a string into last name box.")
+            End If
+            Return True
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Input Error")
+            lNameBox.Focus()
+            Return False
+        End Try
+    End Function
+    Private Function CheckAge(ByRef age As Integer) As Boolean
+        Try
+            age = Integer.Parse(ageBox.Text)
+            If ageBox.Text < 0 Then
+                Throw New Exception("Age cannot be less than 0.")
+            End If
+            Return True
+        Catch ex As FormatException
+            MessageBox.Show("Error in the Age.", ex.GetType.ToString)
+            ageBox.Focus()
+            Return False
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Age cannot be less than 0.")
+            ageBox.Focus()
+            Return False
+        End Try
+    End Function
+
+    Private Function CheckAgeNoArg() As Boolean
+        Dim age As Integer
+        Return CheckAge(age)
+    End Function
+
+
+End Class
+
